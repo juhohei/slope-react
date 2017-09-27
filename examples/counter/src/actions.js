18b9fe6fd8410104e28ab6a7422654d92a513af4
Juho Heinisuo <juho.heinisuo@iki.fi>
2017-09-26 22:46:09 -0400

    Move to one file for better overview

--- examples/counter/src/actions.js
+++
@@ -1,9 +0,0 @@
-import {Action} from 'slope'
-
-const count  = Action()
-const search = Action()
-
-export {
-  count,
-  search
-}
--- examples/counter/src/main.js
+++ examples/counter/src/main.js
@@ -1,10 +1,27 @@
-import React    from 'react'
-import {render} from 'react-dom'
+import React       from 'react'
+import {render}    from 'react-dom'
+import {Action, S} from 'slope'
+import {lift}      from '../../../dist'
 
-import App from './App'
+
+const count   = Action()
+const countS  = S.fork(count.stream)
+const counter = S.scan((p, n) => p + n, 0)(countS)
+const clicks  = S.scan(p => p + 1, 0)(countS)
+
+const App = () => (
+  <main>
+    <p>Current value of the counter is {lift(counter)} and the controls have been clicked {lift(clicks)} times.</p>
+    <div>
+      <button onClick={() => count(1)}>+</button>
+      <button onClick={() => count(-1)}>-</button>
+    </div>
+  </main>
+)
 
 render(
   <App/>,
   document.getElementById('app')
 )
+
 
